551c100a06c1211d43f1f48e8032bc4deca0726d
arunavabanerjee <mrarunbanerjee09@gmail.com>
2018-01-26 11:24:55 +0530

    Create libbackbonejs.js

---
+++ libbackbonejs.js
@@ -0,0 +1,13 @@
+/**
+ * ref: https://www.tutorialspoint.com/backbonejs/index.htm
+ * BackboneJS is a light weight JavaScript library that allows to develop and structure client side applications 
+ * that run in a web browser. It offers MVC framework which abstracts 
+ * data into models, DOM (Document Object Model) into views 
+ * and bind these two using events.
+ * BackboneJS was developed by Jeremy Ashkenas and was initially released on October 13th, 2010 
+ * It manages the data model which includes the user data and displays that data at the server side with the same 
+ * format written at the client side. 
+ * BackboneJS has a soft dependency with jQuery and a hard dependency with Underscore.js.
+ */
+
+ 
