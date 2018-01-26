adfefcbced6b24e180ca0ba835e67e623ed975de
arunavabanerjee <mrarunbanerjee09@gmail.com>
2018-01-26 23:37:57 +0530

    Update libbackbonejs.js

--- libbackbonejs.js
+++ libbackbonejs.js
@@ -20,4 +20,22 @@ jQuery(document).ready(function($){ alert('this'); });
     "use strict";  
     // javascript code here. i.e.: $(document).ready( function(){} ); 
 })(jQuery);
+var AppView = Backbone.View.extend ({
+   // el - stands for element. Every view has an element associated with HTML content, will be rendered. 
+   el: '#content',
+            
+   // It's the first function called when this view is instantiated.
+   initialize: function() {
+     this.render(); 
+   },
+            
+   // $el - it's a cached jQuery object (el), in which you can use jQuery functions to push content.
+            
+   //Like the Hello TutorialsPoint in this case.
+   render: function() {
+     //this.$el.html("Hello TutorialsPoint!!!");
+     this.$el.append("Hello TutorialsPoint!!!");
+   }
+});
+var appView = new AppView();
  
